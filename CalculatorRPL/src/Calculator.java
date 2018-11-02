e599ea156bc460139e25ee4e63ca0a12b667bd24
rrrr_98 <mugen.nikki22@gmail.com>
2018-11-02 14:00:30 +0700

    plus minus implementation

--- CalculatorRPL/src/Calculator.java
+++ CalculatorRPL/src/Calculator.java
@@ -15,11 +15,11 @@ public class Calculator {
     }
     
     public float plus(float a, float b) {
-        
+        return a + b;
     }
     
     public float minus(float a, float b) {
-        
+        return a - b;
     }
     
     public float multiply(float a, float b) {
