5a15ef7d6af2b48184e4c962335049cfe9df1d9c
rrrr_98 <mugen.nikki22@gmail.com>
2018-11-02 13:52:31 +0700

    init commit

---
+++ CalculatorRPL/src/Calculator.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bakazero
+ */
+public class Calculator {
+    
+    public Calculator() {
+        
+    }
+    
+    public float plus(float a, float b) {
+        
+    }
+    
+    public float minus(float a, float b) {
+        
+    }
+    
+    public float multiply(float a, float b) {
+        
+    }
+    
+    public float division(float a, float b) {
+        
+    }
+}
